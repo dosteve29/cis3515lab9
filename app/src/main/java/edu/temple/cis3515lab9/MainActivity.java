1a03a3ac16a9a9c046b4781dc1940259f0c41c82
Hyunseung Do <gustmd1210@gmail.com>
2018-04-24 23:21:24 -0400

    Finished Design/Layout.

--- app/src/main/java/edu/temple/cis3515lab9/MainActivity.java
+++ app/src/main/java/edu/temple/cis3515lab9/MainActivity.java
@@ -1,7 +1,13 @@
 package edu.temple.cis3515lab9;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        Fragment fragment = new PortfolioFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().add(R.id.fragContainer, fragment).commit();
+    }
+
+
+    //create the toolbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.search_stock);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        return super.onCreateOptionsMenu(menu);
     }
 }
